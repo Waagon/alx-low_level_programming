45e6d33633b97af706eda8962678f52258db17d2
Waagon <juliananti.35@gmail.com>
2023-04-24 04:33:50 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: Always 0
+  */
+
+int main(void)
+{
+
+	int m, n;
+
+	for (m = 0; m <= 98; m++)
+	{
+		for (n = m + 1; n <= 99; n++)
+		{
+			putchar((m / 10) + '0');
+				putchar((m % 10) + '0');
+				putchar(' ');
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+				if (m == 98 && n == 99)
+					continue;
+				putchar(',');
+				putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
