14fe21ea38b4e0e0d8f7feba781e5109fb79d059
Waagon <juliananti.35@gmail.com>
2023-04-28 14:37:08 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_numbers - print 0-9
+  * return: void
+  */
+void print_numbers(void)
+{
+	char b;
+
+	for (b = '0';  b <= '9'; b++)
+	{
+	_putchar(b);
+	}
+	_putchar('\n');
+}
