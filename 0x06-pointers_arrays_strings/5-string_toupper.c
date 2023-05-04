052fb20744238a4a1fcad9021715350db29e1bcc
Waagon <juliananti.35@gmail.com>
2023-05-04 11:49:41 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * string_toupper - changes lowercase to uppercase
+  * @n: pointer
+  * Return: char
+  */
+char *string_toupper(char *n)
+{
+	int k;
+
+	k = 0;
+	while (n[k] != '\0')
+	{
+		if (n[k] >= 'a' && n[k] <= 'z')
+			n[k] = n[k] - 32;
+		k++;
+	}
+	return (n);
+}
