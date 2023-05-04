a505555102c1cc509fbbe35f5b5ee02306b5d7fb
Waagon <juliananti.35@gmail.com>
2023-05-04 12:33:33 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * cap_string - capitalizes all words of a string
+  * @str: string
+  * Return: pointer to string
+  */
+char *cap_string(char *str)
+{
+	int k = 0;
+
+	while (str[k])
+	{
+	while (!(str[k] >= 'a' && str[k] <= 'z'))
+	k++;
+	if (str[k - 1] == ' ' ||
+	str[k - 1] == '\t' ||
+	str[k - 1] == '\n' ||
+	str[k - 1] == ',' ||
+	str[k - 1] == ';' ||
+	str[k - 1] == '.' ||
+	str[k - 1] == '!' ||
+	str[k - 1] == '?' ||
+	str[k - 1] == '"' ||
+	str[k - 1] == '(' ||
+	str[k - 1] == ')' ||
+	str[k - 1] == '{' ||
+	str[k - 1] == '}' ||
+	k == 0)
+	str[k] -= 32;
+	k++;
+	}
+	return (str);
+}
