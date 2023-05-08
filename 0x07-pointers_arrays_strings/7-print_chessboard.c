95d9ad4daee6db4044c2cf9bec1586020c7392b4
Waagon <juliananti.35@gmail.com>
2023-05-08 13:31:00 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_chessboard - function that prints chessboard
+  * @a: array
+  * Return: Always 0(Success)
+  */
+void print_chessboard(char (*a)[8])
+{
+	int k;
+	int l;
+
+	for (k = 0; k < 8; k++)
+	{
+		for (l = 0; l < 8; l++)
+			_putchar(a[k][l]);
+		_putchar('\n');
+	}
+}
