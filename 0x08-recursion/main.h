7766339b1c5233c9014ea1bda79fc94f9db5b016
Waagon <juliananti.35@gmail.com>
2023-05-10 16:23:16 -0700

    Task 8

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,5 +16,6 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int actual_sqrt_recursion(int n, int i);
 int actual_prime(int n, int i);
+int check_pal(char *s, int i, int len);
 
 #endif
