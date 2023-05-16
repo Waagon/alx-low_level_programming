ac367c34b953d8bb358be464231fc8b257def97a
Waagon <juliananti.35@gmail.com>
2023-05-16 16:51:50 -0700

    Task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * argstostr - converts the params passed to the program to string
+  * @ac: the argument count
+  * @av: the argument vector
+  *
+  * Return: ...
+  */
+char *argstostr(int ac, char **av)
+{
+	int ch = 0, m = 0, n = 0, o = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (m < ac)
+	{
+		while (av[m][n])
+		{
+			ch++;
+			n++;
+		}
+
+		n = 0;
+		m++;
+	}
+
+	s = malloc((sizeof(char) * ch) + ac + 1);
+
+	m = 0;
+	while (av[m])
+	{
+		while (av[m][n])
+		{
+			s[o] = av[m][n];
+			o++;
+			n++;
+		}
+
+		s[o] = '\n';
+
+		n = 0;
+		o++;
+		m++;
+	}
+
+	o++;
+	s[o] = '\0';
+	return (s);
+}
