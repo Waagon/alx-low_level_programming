c79295c5bd993f1672ef8809bcc0acde739cef7a
Waagon <juliananti.35@gmail.com>
2023-05-22 13:31:00 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * free_dog - a function that frees dogs
+  * @d: pointer to struct dog to free
+  */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
